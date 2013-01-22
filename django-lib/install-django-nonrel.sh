8852500a61aa9c4198cd1fdb5dfbe6dfdc0415b4
arturo <arturo@interpegasus.com>
2013-01-22 16:43:04 -0500

    Django Nonrel GAE Scripts For Installation and Updating

---
+++ django-lib/install-django-nonrel.sh
@@ -0,0 +1,12 @@
+git clone git://github.com/django-nonrel/django-1.4.git
+git clone git://github.com/django-nonrel/djangoappengine.git
+git clone git://github.com/django-nonrel/djangotoolbox.git
+hg clone https://bitbucket.org/twanschik/django-autoload
+git clone git://github.com/django-nonrel/django-dbindexer.git
+git clone git://github.com/django-nonrel/django-testapp.git ../
+
+ln -s django-1.4 ../django
+ln -s djangoappengine ../djangoappengine
+ln -s djangotoolbox ../djangotoolbox
+ln -s django-autoload ../autoload
+ln -s django-dbindexer ../dbindexer
---
+++ django-lib/update-django-nonrel.sh
@@ -0,0 +1,6 @@
+cd django-1.4 && git pull && cd ..
+cd djangoappengine && git pull && cd ..
+cd djangotoolbox && git pull && cd ..
+cd django-autoload && git pull && cd ..
+cd django-dbindexer && git pull && cd ..
+cd django-autoload  && hg pull -u && cd ..
