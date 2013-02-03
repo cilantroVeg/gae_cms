42e07debdbe4c74fda1789bbc99ff11db1d4dbcd
arturo <arturo@interpegasus.com>
2013-02-02 23:49:59 -0500

    NonRel Working Ok

--- lib/install-django-nonrel.sh
+++
@@ -1,6 +0,0 @@
-git clone git://github.com/django-nonrel/django-nonrel.git django
-git clone git://github.com/django-nonrel/djangoappengine.git
-git clone git://github.com/django-nonrel/djangotoolbox.git
-hg clone https://bitbucket.org/twanschik/django-autoload
-git clone git://github.com/django-nonrel/django-dbindexer.git
-git clone git://github.com/django-nonrel/django-testapp.git
--- lib/update-django-nonrel.sh
+++
@@ -1,6 +0,0 @@
-cd django && git pull && cd ..
-cd djangoappengine && git pull && cd ..
-cd djangotoolbox && git pull && cd ..
-cd django-autoload && git pull && cd ..
-cd django-dbindexer && git pull && cd ..
-cd django-autoload  && hg pull -u && cd ..
