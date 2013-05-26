8b06288d71b4118e3cd6de14a22011950e9888d9
arturo <arturo@interpegasus.com>
2013-05-25 21:27:58 -0400

    Default user Authentication Ok

--- projects/cms/users/views.py
+++ projects/cms/users/views.py
@@ -9,6 +9,7 @@ from django.template import RequestContext
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout
+from django.contrib.auth import login
 #from django.contrib.auth.models import Group, Permission
 #from django.contrib.contenttypes.models import ContentType
 
@@ -40,8 +41,6 @@ def not_found(request):
 
 def exit_request(request):
     logout(request)
-    request.session['is_logged_in'] = False
-    request.session['email'] = None
     return redirect('/', False)
 
 
@@ -56,10 +55,14 @@ def process_create_account(request):
         return render_to_response('users/signup.html', {'error_message': "Email is already taken"},
                                   context_instance=RequestContext(request))
     User.objects.create_user(request.POST['email'], request.POST['email'], request.POST['password'])
-    request.session['is_logged_in'] = True
-    request.session['email'] = request.POST['email']
-    request.session['user_id'] = User.id
-    return redirect('/', False)
+    user = authenticate(username=request.POST['email'], password=request.POST['password'])
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('/', False)
+    else:
+        return render_to_response('users/signup.html', {'error_message': "Email is already taken"},
+                                  context_instance=RequestContext(request))
 
 
 @require_http_methods(["POST"])
@@ -70,9 +73,6 @@ def process_sign_up(request):
     if user is not None:
         if user.is_active:
             login(request, user)
-            request.session['is_logged_in'] = True
-            request.session['email'] = request.POST['email_2']
-            # Redirect to a success page.
             return redirect('/', False)
     else:
         # Return an 'invalid login' error message.
@@ -81,8 +81,6 @@ def process_sign_up(request):
 
 
 def front_page(request):
-    for key, value in request.session.items(): # .iteritems() in Python 2.x
-        pprint.pprint(value)
     if is_logged_in(request) is False:
         return render_to_response('users/signup.html', {'is_logged_in': is_logged_in(request)},
                                   context_instance=RequestContext(request))
@@ -100,10 +98,6 @@ def my_custom_500_view(request):
                               context_instance=RequestContext(request))
 
 # Social Auth
-def logged_in(request):
-    request.session['is_logged_in'] = True
-    return redirect('/', False)
-
 def login_error(request):
     return render_to_response('users/signup.html', {'error_message': "Incorrect login. Please try again"},
                               context_instance=RequestContext(request))
