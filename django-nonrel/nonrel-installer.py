ed62159391a733105a6e1efc2933470f2e9a8f20
arturo <arturo@mobovivo.com>
2013-07-01 12:22:23 -0400

    Update

---
+++ django-nonrel/nonrel-installer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import subprocess
+
+def urls_master():
+    urls_master = [
+        "rm -rf django*"
+        "git clone https://github.com/django-nonrel/django.git",
+        "git clone https://github.com/django-nonrel/djangoappengine.git",
+        "git clone https://github.com/django-nonrel/djangotoolbox.git",
+        "hg clone https://bitbucket.org/twanschik/django-autoload",
+        "git clone https://github.com/django-nonrel/django-dbindexer.git",
+        "git clone https://github.com/django-nonrel/django-testapp.git"
+    ]
+    return urls_master
+
+def project_list():
+    project_list = [
+        'cms',
+        'arturo',
+        'interpegasus',
+        'magicangel',
+        'music',
+        'nrwl',
+        'recipes',
+        'yiyask'
+    ]
+
+
+def main():
+    url_array = urls_master()
+    for url in url_array:
+        subprocess.call(url, shell=True)
+
+    project_list= project_list()
+    subprocess.call("cd ../projects", shell=True)
+
+    for project in project_list:
+        subprocess.call("mkdir " + project, shell=True)
+
+    for project in project_list:
+        subprocess.call("ln -s ../django-nonrel/django/django " + project + "/", shell=True)
+        subprocess.call("ln -s ../django-nonrel/djangotoolbox/djangotoolbox " + project + "/", shell=True)
+        subprocess.call("ln -s ../django-nonrel/django-autoload/autoload " + project + "/", shell=True)
+        subprocess.call("ln -s ../django-nonrel/django-dbindexer/dbindexer " + project + "/", shell=True)
+        subprocess.call("ln -s ../django-nonrel/djangoappengine/djangoappengine " + project + "/", shell=True)
+
+# Installs the base system for django-nonrel
+main()
