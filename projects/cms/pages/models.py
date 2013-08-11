bfa9f4fd7d734e8d52ed1fae0f0c315e296cd1be
arturo <arturo@interpegasus.com>
2013-08-11 03:33:49 -0400

    Template Footer Structure OK Caching OK

--- projects/cms/pages/models.py
+++ projects/cms/pages/models.py
@@ -26,7 +26,7 @@ class Language(models.Model):
 class Category(models.Model):
     ORDER = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'), (6, '6'), (7, '7'))
     id = models.AutoField(primary_key=True)
-    language = models.ForeignKey(Language, null=False, blank=False)
+    language = models.ForeignKey(Language, null=True, blank=True)
     name = models.CharField(max_length=256, null=False, blank=False)
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
     slug = models.SlugField(unique=True, blank=False, null=False)
