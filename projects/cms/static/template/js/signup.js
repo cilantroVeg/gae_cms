c097862665cccdc1765db8c636732bd66318f628
arturo <arturo@interpegasus.com>
2013-09-02 17:16:26 -0700

    Templates

---
+++ projects/cms/static/template/js/signup.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	$("#create_user_form").validate({
+		rules : {
+			email : {
+				email : true,
+				required : true
+			},
+			confirm_email : {
+				email : true,
+				required : true,
+				equalTo : "#email"
+			},
+			password : {
+				required : true,
+				minlength : 7
+			},
+			confirm_password : {
+				required : true,
+				minlength : 7,
+				equalTo : "#password"
+			}
+        }
+	});
+
+    $("#contact_form").validate({
+		rules : {
+			contact_email : {
+				email : true,
+				required : true
+			},
+			contact_name : {
+				required : true,
+				minlength : 7
+			},
+			contact_comment : {
+				required : true,
+				minlength : 21
+			}
+		}
+	});
+
+	$("#login_user_form").validate({
+		rules : {
+			email_2 : {
+				email : true,
+				required : true
+			},
+			password_2 : {
+				required : true,
+				minlength : 7
+			}
+		}
+	});
+
+});
