32272dc3dade9c64fb29bd662ce9849d3c056cd5
arturo <arturo@interpegasus.com>
2013-09-03 22:15:55 -0700

    MAF OK

--- projects/cms/static/template/www.magicangel.org/js/main.js
+++ projects/cms/static/template/www.magicangel.org/js/main.js
@@ -73,5 +73,34 @@ $(document).ready(function(){
         controlNav: true,               //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
         directionNav: true             //Boolean: Create navigation for previous/next navigation? (true/false)
     });
+    $("#menu-tab").click(function(){
+        if ($("#menu-container").is(':visible')){
+            $("#menu-tab").css("marginLeft", "200").animate({
+                    marginLeft: "0"
+                }, 1000, function() {}
+            );
+            $("#menu-container").css("marginLeft", "0").animate({
+                    marginLeft: -200
+                }, 1000, function() {
+                    $("#menu-container").css('display', 'none');
+                }
+            );
+        } else{
+            $("#menu-tab").css("marginLeft", "0").animate({
+                    marginLeft: 200
+                }, 1000, function() {}
+            );
+            $("#menu-container").css('display', 'block');
+            $("#menu-container").css("marginLeft", "-200").animate({
+                    marginLeft: 0
+                }, 1000, function() {
+
+                }
+            );
+        }
+    });
+    $(function() {
+        $( "#menu" ).menu();
+    });
 
 });
