9ae6647d71549169ae86417baaed6dbdac320013
arturo <arturo@interpegasus.com>
2014-02-12 09:07:01 -0500

    Bible Template

--- projects/cms/settings.py
+++ projects/cms/settings.py
@@ -151,12 +151,12 @@ SOCIAL_AUTH_PIPELINE = (
 
 LOGIN_REDIRECT_URL = '/'
 
-APP_NAME = 'interpegasus-cms'
+#APP_NAME = 'interpegasus-cms'
 #APP_NAME = 'interpegasuslove'
 #APP_NAME = 'happy-planet'
 #APP_NAME = 'arturopegasus7'
 #APP_NAME = 'arturoportfolio7'
-#APP_NAME = 'bible-love'
+APP_NAME = 'bible-love'
 #APP_NAME = 'musicmatch777'
 #APP_NAME = 'yiyask7'
 
