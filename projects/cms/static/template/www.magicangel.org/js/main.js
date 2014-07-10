23070c908429165dcdcd03b4800c8e108f17a2d2
arturo <arturo@interpegasus.com>
2014-07-10 10:26:26 -0500

    Admin Menu In Progress

--- projects/cms/static/template/www.magicangel.org/js/main.js
+++ projects/cms/static/template/www.magicangel.org/js/main.js
@@ -76,35 +76,4 @@ $(document).ready(function(){
     }catch (err){
 
     }
-
-    $("#menu-tab").click(function(){
-        if ($("#menu-container").is(':visible')){
-            $("#menu-tab").css("marginLeft", "200").animate({
-                    marginLeft: "0"
-                }, 1000, function() {}
-            );
-            $("#menu-container").css("marginLeft", "0").animate({
-                    marginLeft: -200
-                }, 1000, function() {
-                    $("#menu-container").css('display', 'none');
-                }
-            );
-        } else{
-            $("#menu-tab").css("marginLeft", "0").animate({
-                    marginLeft: 200
-                }, 1000, function() {}
-            );
-            $("#menu-container").css('display', 'block');
-            $("#menu-container").css("marginLeft", "-200").animate({
-                    marginLeft: 0
-                }, 1000, function() {
-
-                }
-            );
-        }
-    });
-    $(function() {
-        $( "#menu" ).menu();
-    });
-
 });
