adfc293bda3972ce982a37f8da218f023b50b381
arturo <arturo@interpegasus.com>
2014-07-14 17:44:51 -0500

    Deploy OK

--- projects/cms/pages/views.py
+++ projects/cms/pages/views.py
@@ -518,7 +518,7 @@ def front_page(request):
 def front_page_language(request,language):
     image_array = Image.objects.all()[:7]
     language_code = 'en' if (language is None) else language
-    feed_pages = query_api(language, 'feed_pages')
+    feed_pages = query_api(language_code, 'feed_pages')
     return render_to_response('users/front_page.html', {'feed_pages':feed_pages['pages'], 'image_array': image_array,'is_admin':is_admin(request)['is_admin']}, context_instance=RequestContext(request))
 
 # Custom 404 and 500
