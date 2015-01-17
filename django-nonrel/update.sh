efdf594f262a89a7676336f4f0de1d142cbae779
arturo <arturo@interpegasus.com>
2015-01-17 08:59:06 -0500

    update

--- django-nonrel/update.sh
+++ django-nonrel/update.sh
@@ -1,20 +1,45 @@
+echo '**********************************'
+echo '* AUTOLOAD                       *'
+echo '**********************************'
 cd autoload
+git reset --hard origin/master
+git status
 git branch
 git pull
 cd ..
+echo '**********************************'
+echo '* DJANGO                         *'
+echo '**********************************'
 cd django
+git reset --hard origin/master
+git status
 git branch
 git pull
 cd ..
+echo '**********************************'
+echo '* DBINDEXER                      *'
+echo '**********************************'
 cd django-dbindexer
+git reset --hard origin/master
+git status
 git branch
 git pull
 cd ..
+echo '**********************************'
+echo '* DJANGOAPPENGINE                *'
+echo '**********************************'
 cd djangoappengine
+git reset --hard origin/master
+git status
 git branch
 git pull
 cd ..
+echo '**********************************'
+echo '* DJANGOTOOLBOX                  *'
+echo '**********************************'
 cd djangotoolbox
+git reset --hard origin/master
+git status
 git branch
 git pull
 cd ..
