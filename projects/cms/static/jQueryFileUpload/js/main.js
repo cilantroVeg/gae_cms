4c03ae7a066113ca228ef83f46bc1d75ff32c2bf
arturo <arturo@interpegasus.com>
2015-01-28 19:46:02 -0500

    File Upload OK

--- projects/cms/static/jQueryFileUpload/js/main.js
+++ projects/cms/static/jQueryFileUpload/js/main.js
@@ -18,7 +18,9 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        url: '/upload_handler/'
+        url: '/upload_handler/',
+        limitConcurrentUploads: 2,
+        limitMultiFileUploads: 1
     });
 
     // Enable iframe cross-domain access via redirect option:
