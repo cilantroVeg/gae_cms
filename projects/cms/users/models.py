79833957e7b574992dd87e87ff3aeb4df722ec61
arturo <arturo@interpegasus.com>
2015-02-21 22:47:50 -0500

    User Profiles

--- projects/cms/users/models.py
+++ projects/cms/users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from social.backends import google
+from social.backends import facebook
+from social.backends import twitter
 
 
 class UserProfile(models.Model):
@@ -31,3 +34,37 @@ class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = ['user', 'profile_picture']
+
+
+def social_extra_values(sender, user, response, details, **kwargs):
+    if "id" in response:
+        from urllib2 import urlopen, HTTPError
+        from django.template.defaultfilters import slugify
+        from django.core.files.base import ContentFile
+
+        try:
+            url = None
+            if sender == facebook:
+                url = "http://graph.facebook.com/%s/picture?type=large" \
+                            % response["id"]
+            elif sender == google.GoogleOAuth2Backend and "picture" in response:
+                url = response["picture"]
+
+            elif sender == twitter:
+                url = response["profile_image_url"]
+
+            if url:
+                avatar = urlopen(url)
+                profile = UserProfile(user=user)
+
+                profile.profile_picture.save(slugify(user.username + " social") + '.jpg',
+                        ContentFile(avatar.read()))
+
+                profile.save()
+
+        except HTTPError:
+            pass
+
+
+
+    return False
