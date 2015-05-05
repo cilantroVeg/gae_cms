dab989751e4494918fe79175083dbb1eb5d57c0a
arturo <arturo@interpegasus.com>
2015-05-05 09:37:54 -0500

    Fix Layout

--- projects/cms/settings.py
+++ projects/cms/settings.py
@@ -160,7 +160,7 @@ APP_NAME = 'interpegasuslove'
 
 CACHE_ENABLED = True
 
-if False:
+if True:
     SITE_URL = 'http://127.0.0.1:8080'
 elif APP_NAME == 'interpegasus-cms':
     SITE_URL = 'http://www.magicangel.org'
